fc05711b67ddc708663fdbd957d99d1de5be0cd1
Joe Dralliam <jdralliam@gmail.com>
2015-05-23 18:38:28 +0200

    Add a readme

---
+++ mapReduce.zsh
@@ -0,0 +1,10 @@
+export TIMEFMT=$'%E'
+for w in 1 3 5 10 20 50 100 200
+do
+    printf "$w workers:\n" ;
+    for x in "sequentiel" "pipes" "pipes-thread" "thread" "network" "network-thread"
+    do
+	printf "$x:" ;
+	time timeout 30 ./mapReduce.native -workers $w -silent -network $x;
+    done
+done
